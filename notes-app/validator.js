cd35595b2e9a584a73165b1500fcfa5392d396b2
vkumar.kumar <vkumar112@sapient.com>
2019-11-14 12:52:04 +0530

    Steps in README.md and created one js to test my skill

---
+++ notes-app/validator.js
@@ -0,0 +1,5 @@
+const validator = require('validator')
+
+console.log(validator.isEmail('vikrnt@com'))
+console.log(validator.isEmail('vikrant@gmail.com'))
+console.log(validator.isURL('https://vikrant.com'))
